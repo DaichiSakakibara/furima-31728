980e8b0b02a16ec71adfd370cf3ddd1c7c3650f7
DaichiSakakibara <dsdaitidsds1226@gmail.com>
2020-11-06 11:33:22 +0900

    Rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true, length: { maximum: 6 }
-         validates :first_name , presence: true
-         validates :last_name , presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birth_day, presence: true
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :birth_day, presence: true
 
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
