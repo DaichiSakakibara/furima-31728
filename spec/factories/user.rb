b7992491ce67eb4e4239f9212d9d536e0ee67271
DaichiSakakibara <dsdaitidsds1226@gmail.com>
2020-11-06 10:24:28 +0900

    単体テストコード記述

---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    first_name            {Faker::Name.initials(number: 2)}
+    last_name             {Faker::Name.initials(number: 2)}
+    first_name_kana       {Faker::Name.initials(number: 2)}
+    last_name_kana        {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    birth_day             { Date.new(2020, 11, 5) }
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+  end
+end
