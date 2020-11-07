e6c06f25178c45b6a7367a30151e12d454a10527
DaichiSakakibara <dsdaitidsds1226@gmail.com>
2020-11-07 10:26:35 +0900

    new&createのルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,26 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path
+     else
+      render :new
+    end
+  end
+
+  private
+
+  def prototype_params
+    params.require(:item).permit(:title, :text, :category_id, :item_status_id, :which_pay_id, :prefectures_id, :how_long_days_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   has_many :items
+
+
+
   with_options presence: true do
     validates :nickname, length: { maximum: 6 }
     validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/}
@@ -15,4 +19,5 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
+
 end
